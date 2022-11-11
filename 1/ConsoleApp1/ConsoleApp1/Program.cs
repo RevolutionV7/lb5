8084c10109b0e3421aabb1a5f9830eed98b45c58
Revolution_V7 <sdvsvd11@gmail.com>
2022-11-11 18:44:28 +0200

    1 task finish

--- 1/ConsoleApp1/ConsoleApp1/Program.cs
+++ 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-
-namespace ConsoleApp1
+namespace Task1
 {
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class Company : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            base.Print(value);
+        }
+    }
+
+    class Position : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            base.Print(value);
+        }
+    }
+
+    class NamePerson : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(value);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Printer name = new NamePerson();
+            name.Print("Dmytro Sosnivskyi");
+            Printer company = new Company();
+            company.Print("IONAR");
+            Printer position = new Position();
+            position.Print("Executice manager");
         }
     }
 }
